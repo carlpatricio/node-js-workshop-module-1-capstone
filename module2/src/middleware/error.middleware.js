25b274010498a09a627fa99d4c5f399675e324e9
Carl Patricio <carlpatricio23@gmail.com>
2023-10-22 08:08:12 +0800

    feat(error.middleware.js): update stackTrace exclusivity to local env/dev only

--- module2/src/middleware/error.middleware.js
+++ module2/src/middleware/error.middleware.js
@@ -2,9 +2,13 @@ import { logger } from "../util/logger.js";
 
 export const handleError = (err, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    /**
+     * stacktrace will only be included in local development - dev
+     */
+    const stackTrace = process.env.NODE_ENV === 'dev' ? err.stack : undefined;
     res.status(statusCode).json({
         message: err.message,
-        stackTrace: err.stack
+        stackTrace
     });
-    logger.error(`${statusCode} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+    logger.error(`${statusCode} - ${err.message} - ${req.originalUrl} - ${req.method} - ${err.stack}`);
 };
